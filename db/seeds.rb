c2a9358db56d1dad47fdc9025c4d1a1b8c9be395
Ryan <ryanimmesberger@gmail.com>
2015-04-06 17:02:39 -0400

    added ingredients and recipe/ingredients tables models controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,14 @@ toria = User.create(name: 'Toria', password: '1234')
 
 chicken_pot_pie = Recipe.create(title: 'Chicken Pot Pie', instruction: 'Mush it together and bake for 45 minutes.', user: ryan)
 ravioli = Recipe.create(title: 'Ravioli', instruction: 'throw some jared sauce on there and eat it', user: toria)
+chicken_parm = Recipe.create(title: "Chicken Parm", instruction: "Bread it, bake it, eat it", user: ryan)
+
+meat_pie = Recipe.create(title: "Meat pie", instruction: "make a crust pocket, fill with meat", user: toria)
+
+Ingredient.create(name: "chicken")
+Ingredient.create(name: "tomato sauce")
+Ingredient.create(name: "chop meat")
+Ingredient.create(name: "dough")
+
+RecipeIngredient.create(recipe: chicken_parm, ingredient: Ingredient.find_by(name: "chicken"), quantity: "2")
+RecipeIngredient.create(recipe: chicken_parm, ingredient: Ingredient.find_by(name: "tomato sauce"), quantity: "1")
