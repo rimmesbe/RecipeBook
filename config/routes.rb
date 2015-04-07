47fb9cb38e274e1c54a54b0ad1eb0981f26a3122
Ryan <ryanimmesberger@gmail.com>
2015-04-07 17:08:19 -0400

    created ingredients routes and views, all models are now linked

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :recipes
   resources :users
+  resources :ingredients
+
   resource :login do
-    get '/logout'
+    get '/logout' => 'user#logout'
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
